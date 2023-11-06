a207b3e51591975aee508b846193032cb94e5f16
Ramin Moussavi <lordrasmus@gmail.com>
2023-11-06 12:25:31 +0100

    qemu test 10

---
+++ .github/tools/post_badge.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python3
+
+# pip3 install pybadges
+
+import sys
+import requests
+import json
+
+
+import requests
+
+
+from pprint import pprint
+
+github_token = sys.argv[1]
+file_name = sys.argv[2]
+gist_id = "None"
+
+"""
+
+# mit diesem code kann man die id zu einer datei finden
+
+# Ersetzen Sie 'Benutzername' und 'Gist-ID' durch die entsprechenden Werte
+gist_owner = 'lordrasmus'
+gist_id = "None"
+
+# GitHub API-URL für den Gist
+headers = {
+    "Authorization": "Bearer " + f"{github_token}",
+    "Accept": "application/vnd.github+json",
+    "X-GitHub-Api-Version":"2022-11-28"
+}
+gist_api_url = f'https://api.github.com/gists'
+
+#pprint( headers )
+
+# Führen Sie eine GET-Anfrage zur GitHub API durch
+response = requests.get(gist_api_url, headers=headers )
+
+if response.status_code == 200:
+    gist_data = response.json()
+    for f in gist_data:
+        #print( f["id"] )
+        #print( f["files"].keys() )
+        
+        if file_name in f["files"].keys():
+            gist_id = f["id"]
+            
+            
+    # Gist-ID aus der Antwort extrahieren
+    #gist_id = gist_data['id']
+    #print("Gist-ID:", gist_id)
+else:
+    print(f"Fehler beim Abrufen der Gist-Daten. Statuscode: {response.status_code}")
+    
+"""
+
+gist_id = "867aa95ade60fa5b1ad098fa6c6a1968"
+
+print( "ID: " + gist_id )
+
+
+
+if gist_id == "None":
+
+    #
+    #   wenn es noch keine id gibt kann man so eine neue datei anlegen
+    #   und bekommt dann die id zurück
+    #
+
+    # Ersetzen Sie die folgenden Werte durch Ihre eigenen Informationen
+
+    gist_description = "Mein Gist"
+
+    f = open("badge.svg","r")
+    file_content = f.read()
+    f.close()
+
+    # Gist-Payload erstellen
+    payload = {
+        "description": gist_description,
+        "public": True,
+        "files": {
+            file_name: {
+                "content": file_content
+            }
+        }
+    }
+
+    # Gist erstellen
+    url = "https://api.github.com/gists"
+    headers = {
+        "Authorization": f"token {github_token}"
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+    # Prüfen, ob die Anfrage erfolgreich war
+    if response.status_code == 201:
+        gist_data = response.json()
+        gist_id = gist_data["id"]
+        gist_url = gist_data["html_url"]
+        print(f"Erstellter Gist-ID: {gist_id}")
+        print(f"Gist URL: {gist_url}")
+    else:
+        print(f"Fehler bei der Erstellung des Gists. Statuscode: {response.status_code}")
+        print(response.text)
+
+else:
+    
+    # Ersetzen Sie die folgenden Werte durch Ihre eigenen Informationen
+
+    gist_description = "Mein Gist"
+
+    f = open("badge.svg","r")
+    file_content = f.read()
+    f.close()
+
+    # Gist-Payload erstellen
+    payload = {
+        "description": gist_description,
+        "public": True,
+        "files": {
+            file_name: {
+                "content": file_content
+            }
+        }
+    }
+
+    # Gist erstellen
+    url = "https://api.github.com/gists/"+ gist_id
+    headers = {
+        "Authorization": f"Bearer {github_token}",
+        "X-GitHub-Api-Version":"2022-11-28"
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+    # Prüfen, ob die Anfrage erfolgreich war
+    if response.status_code == 200:
+        gist_data = response.json()
+        gist_id = gist_data["id"]
+        gist_url = gist_data["html_url"]
+        print(f"Erstellter Gist-ID: {gist_id}")
+        print(f"Gist URL: {gist_url}")
+    else:
+        print(f"Fehler bei der Erstellung des Gists. Statuscode: {response.status_code}")
+        print(response.text)
--- .github/tools/read_test_log.py
+++ .github/tools/read_test_log.py
@@ -4,15 +4,21 @@ import sys
 import time
 import re
 
+from pybadges import badge
+
 from junit_xml import TestSuite, TestCase
 
 test_cases = []
+
+test_results = {"TOTAL": 0, "PASS": 0 , "FAIL":0 , "SKIP": 0 }
 
 with open("log.txt","r") as f:
     for line in f.readlines():
         #print( l )
         
         if 'PASS ' in line:
+            test_results["TOTAL"] += 1
+            test_results["PASS"] += 1
             r = re.match("PASS (.*)", line)
             if r:
                 test_name = r.group(1).split()[0]
@@ -20,6 +26,8 @@ with open("log.txt","r") as f:
                 test_cases.append(test)
 
         if 'FAIL ' in line:
+            test_results["TOTAL"] += 1
+            test_results["FAIL"] += 1
             r = re.match("FAIL (.*)", line)
             if r:
                 failure_line = r.group(1).split(' ', 1)
@@ -31,6 +39,8 @@ with open("log.txt","r") as f:
                 test_cases.append(test)
 
         if 'SKIP' in line:
+            test_results["TOTAL"] += 1
+            test_results["SKIP"] += 1
             r = re.match("SKIP (.*)", line)
             if r:
                 test_name = r.group(1).split()[0]
@@ -46,3 +56,43 @@ with open("log.txt","r") as f:
 ts = TestSuite("uClibc-ng testsuite", test_cases)
 with open("test_result.xml", "w+") as to:
     to.write(TestSuite.to_xml_string([ts]))
+
+
+"""
+
+   create badge.svg
+
+"""
+
+
+#test_results["FAIL"] = 0
+
+right_color='green'
+if test_results["FAIL"] > 0:
+    right_color='#800000'
+
+text = 'total: {0} pass: {1}  failed: {2}  skip: {3}'.format( test_results["TOTAL"], test_results["PASS"], test_results["FAIL"], test_results["SKIP"] )
+
+s = badge(left_text='test results', right_text=text, right_color=right_color )
+
+with open("badge.svg","w") as f:
+    f.write( s )
+
+
+
+"""
+
+   create Test Summary
+
+"""
+
+summary_text = "### Test Summary :bulb:\n"
+summary_text += "|--- |----|\n"
+summary_text += "|Total: {0} |:white_check_mark:|\n".format( test_results["TOTAL"] )
+summary_text += "|Pass: {0} |:white_check_mark:|\n".format( test_results["PASS"] )
+summary_text += "|Fail: {0} |:x:|\n".format( test_results["FAIL"] )
+summary_text += "|skip: {0} |:zzz:|\n".format( test_results["SKIP"] )
+
+
+with open("test_summary.md","w") as f:
+    f.write( summary_text )
